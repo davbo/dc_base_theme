6f627cc5a611599b94a44ff430326471cea3ebf7
Sym Roe <sym.roe@talusdesign.co.uk>
2016-03-14 09:02:24 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='DC Base Theme',
+      version='0.1',
+      description='Base assets for DC projects',
+      author='Sym Roe',
+      author_email='sym.roe@democracyclub.org.uk',
+      packages=['dc_theme'],
+      )
+
+
