712879cac557584337e23c1f862c400bdf088162
Sym Roe <sym.roe@talusdesign.co.uk>
2016-03-14 15:10:31 +0000

    Auto discover data_files

--- setup.py
+++ setup.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python3
+import os
 
 from setuptools import setup
+
+packages = []
+data_files = []
+app_dir = 'dc_theme'
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way. Copied from Django.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+for dirpath, dirnames, filenames in os.walk(app_dir):
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
 
 setup(name='DC Base Theme',
       version='0.1',
       description='Base assets for DC projects',
       author='Sym Roe',
       author_email='sym.roe@democracyclub.org.uk',
-      packages=['dc_theme'],
+      packages=packages,
+      data_files=data_files,
       include_package_data=True,
       )
 
